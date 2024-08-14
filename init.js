a0d5468f5205d08f2d1367a8ba2143c44b2568a4
Harshit Singla <harshitashwani@gmail.com>
2024-08-14 20:34:34 +0530

    Changed save Approach

--- init.js
+++ init.js
@@ -11,7 +11,7 @@ async function main() {
     await mongoose.connect('mongodb://0.0.0.0:27017/whatsapp');
 }
 
-const sampleChat = new Conversation({
+const sampleChat = [{
     recieverName: 'Kartik Sharma',
     recieverDP: 'https://images.pexels.com/photos/220429/pexels-photo-220429.jpeg?cs=srgb&dl=pexels-pixabay-220429.jpg&fm=jpg',
     recieverPhone: '+91 88943 69418',
@@ -23,12 +23,14 @@ const sampleChat = new Conversation({
             text: 'Kaam bhejde',
         }
     ]
-});
+}];
 
-sampleChat.save()
-    .then(savedChat => {
-        console.log('Successfully saved: \n', savedChat);
-    })
-    .catch((error) => {
-        console.error('Error saving chat: \n', error);
-    });
+// sampleChat.save()
+//     .then(savedChat => {
+//         console.log('Successfully saved: \n', savedChat);
+//     })
+//     .catch((error) => {
+//         console.error('Error saving chat: \n', error);
+//     });
+
+Conversation.insertMany(sampleChat);

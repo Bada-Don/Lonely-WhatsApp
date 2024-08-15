e415627eb2b24e30f5ccce63aa6054da4bb22267
Harshit Singla <harshitashwani@gmail.com>
2024-08-15 20:07:56 +0530

    Messages are being sent properly now

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const conversation = require('./models/conversation');
 const moment = require('moment');
+const { url } = require('inspector');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './src')));
+app.use(express.urlencoded({extended: true}));
 
 app.listen(3000, () => {
     console.log('App is listening to port 3000');
@@ -29,6 +31,40 @@ app.get('/', (req, res) => {
         res.render('home.ejs', { chats: formattedChat, activeChat });
     });
 });
+
+// Send New Messages to current chat
+app.post('/', (req, res) => {
+    console.log(req.body); // Log the incoming request body
+    const { conversationId, message } = req.body;
+
+    if (!conversationId || !message) {
+        return res.status(400).send('Missing conversationId or message.');
+    }
+
+    const newMessage = {
+        text: message,
+        date: new Date()
+    };
+
+    // Find the conversation by ID and push the new message to the messages array
+    conversation.findByIdAndUpdate(
+        conversationId,
+        { $push: { messages: newMessage }, lastSeen: new Date() },
+        { new: true }
+    )
+    .then(updatedConversation => {
+        if (!updatedConversation) {
+            return res.status(404).send('Conversation not found.');
+        }
+        console.log("Message added successfully!");
+        res.redirect('/');
+    })
+    .catch(err => {
+        console.error("Error adding message:", err);
+        res.status(500).send("An error occurred while adding the message.");
+    });
+});
+
 
 
 main().then(() => {
